494035fde0a16688b25624e15c1498afcc7fdc89
Umar Shahbaz Khan <umar.sk179@gmail.com>
2021-06-14 23:23:13 +0500

    first-commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -14,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
